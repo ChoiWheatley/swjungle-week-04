858063f32e32919006330cdbc0046eeaea99edbf
ChoiWheatley <chltmdgus604@naver.com>
2023-09-02 17:38:06 +0900

    feat(list) Create test codes

--- basic/list/main.c
+++ basic/list/main.c
@@ -1,16 +1,25 @@
 #include <list.h>
 
 int main(void) {
-  NodePtr first = NULL, second = NULL;
-  MALLOC(first, sizeof(*first));
-  MALLOC(second, sizeof(*second));
+  NodePtr first = NULL;
 
-  first->data = 10;
-  second->data = 20;
-  first->link = second;
-  second->link = NULL;
+  print_list(first);
+
+  insert_left(&first, 10);
+  print_list(first);
+
+  insert_left(&first, 20);
+  print_list(first);
+
+  insert_left(&first, 30);
+  print_list(first);
+
+  insert_left(&first, 40);
+  print_list(first);
+
+  insert_left(&first, 50);
+  print_list(first);
 
   free(first);
-  free(second);
   return EXIT_SUCCESS;
 }
